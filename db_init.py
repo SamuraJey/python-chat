da957cc06c4a801f8bc2f9a4e4a0e6495bcf47c2
Sergei Z. <47861714+SamuraJey@users.noreply.github.com>
2025-04-16 21:11:08 +0500

    Add pytest coverage (#11)

--- db_init.py
+++ db_init.py
@@ -2,7 +2,7 @@ from src.app import create_app
 from src.database import db
 from src.database.db_init import initialize_db
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # pragma: no cover
     app = create_app()
     success = initialize_db(app, db)
     print("Database initialization " + ("successful" if success else "failed"))  # noqa
